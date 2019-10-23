f55dced2aa933820f4efa865294486ee91211911
hagiwara0305 <42800107+Hagiwara35@users.noreply.github.com>
2019-10-23 17:00:06 +0900

    DB追加処理を追加

--- public/html/user-registration.php
+++ public/html/user-registration.php
@@ -1,3 +1,47 @@
+<?php
+session_start();
+
+$error_message = '';
+$sth = null;
+
+if (isset($_POST['user_create'])) {
+    $dsn = 'mysql:dbname=chat;host=127.0.0.1';
+    $user = 'user';
+    $password = 'user';
+
+    try {
+        $dbh = new PDO($dsn, $user, $password);
+
+        $sqlUsername = $_POST['user_name'];
+
+        $sql = 'select * from user where user_name = :user_name';
+        $sth = $dbh->prepare($sql);
+        $sth->bindParam(':user_name', $sqlUsername);
+        $sth->execute();
+
+        if ($sth->rowCount() == 0) {
+            $sqlPassword = hash('ripemd160', $_POST['plain_password']);
+
+            $sql = 'INSERT INTO user (id, user_name, password) VALUES (NULL, :user_name, :password)';
+            $sth = $dbh->prepare($sql);
+            $sth->bindParam(':user_name', $sqlUsername);
+            $sth->bindParam(':password', $sqlPassword);
+            $sth->execute();
+
+            $login_success_url = '../../index.php';
+            header("Location: {$login_success_url}");
+            exit;
+        }
+    } catch (PDOException $e) {
+        $error_message = 'Error:DB接続エラー';
+    } finally {
+        $dbh = null;
+        if(!$error_message){
+            $error_message = "<br>※既にこのユーザ名は使われています。";
+        }
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -6,10 +50,25 @@
 </head>
 <body>
     <h1>ユーザ作成画面</h1>
-    <form>
-        <p>ユーザ名：<input type="text" name="user_name"></p>
+    <form action="user-registration.php" method="POST">
+        <p>
+            ユーザ名：<input type="text" name="user_name">
+            <?php ?>
+        </p>
         <p>パスワード：<input type="text" name="plain_password"></p>
-        <input type="submit" name="user_create">
+        <input type="submit" name="user_create" value="作成">
     </form>
+
+    <div>
+        <?php
+        if (isset($_SESSION["user_name"])) {
+            echo "<a href=\"public/html/logout.php\"><button>ログアウト</button></a>";
+        }
+
+        if ($error_message) {
+            echo $error_message;
+        }
+        ?>
+    </div>
 </body>
 </html>
