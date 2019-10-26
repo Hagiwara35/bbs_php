5f26e79593dd1aa47d437da7fb98979175051e3e
hagiwara0305 <42800107+Hagiwara35@users.noreply.github.com>
2019-10-26 21:12:11 +0900

    スレッド作成のHTMLを追加

---
+++ public/html/sled-create.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>スレッド作成画面</title>
+</head>
+<body>
+    <h1>スレッド作成画面</h1>
+
+    <form action="sled-create.php">
+        <p>スレッド名：<input type="text"></p>
+        <input type="button" value="スレッドを作成">
+    </form>
+</body>
+</html>
