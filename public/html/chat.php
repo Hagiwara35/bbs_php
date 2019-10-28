ebf0fc19139f2773d3b3198299758f155082a1bf
hagiwara0305 <42800107+Hagiwara35@users.noreply.github.com>
2019-10-28 23:52:19 +0900

    コメントboxの位置を変更

--- public/html/chat.php
+++ public/html/chat.php
@@ -26,12 +26,6 @@ $sth = (new DBAccess())->getSQLExecution(
 </head>
 
 <body>
-<!--コメント送信-->
-<div id="comment">
-    <p id="name_print"></p>
-    <textarea id="comment_area" onkeyup="sendMessage(event)" placeholder="コメント" maxlength="255"></textarea>
-</div>
-
 <!--コメント書き込み-->
 <div id="chat">
     <?php
@@ -60,6 +54,13 @@ $sth = (new DBAccess())->getSQLExecution(
     }
     ?>
 </div>
+
+<!--コメント送信-->
+<div id="comment">
+    <p id="name_print"></p>
+    <textarea id="comment_area" onkeyup="sendMessage(event)" placeholder="コメント" maxlength="255"></textarea>
+</div>
+
 <script type="text/javascript" src="../js/comment.js"></script>
 </body>
 </html>
